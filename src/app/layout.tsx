d1a89c759768169cd4c4f6a8c1b396ca5b3bdd64
lixiang90 <lx20150211@gmail.com>
2025-06-07 21:51:39 +0800

    add icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "AI丛刊",
   description: "接受AI生成文章投稿",
+  icons: {
+    icon: '/favicon.png',
+  },
 };
 
 export default function RootLayout({
